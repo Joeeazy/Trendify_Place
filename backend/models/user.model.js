e5358e69269ef946072d0eed038454a0fe89b8b4
Joeeazy <joeeazy34@gmail.com>
2024-09-26 20:46:11 +0300

    UserSchema and Model created

---
+++ backend/models/user.model.js
@@ -0,0 +1,47 @@
+import mongoose from "mongoose";
+
+//create a user schema
+const userSchema = new mongoose.Schema(
+  {
+    name: {
+      type: String,
+      required: [true, "Name is required"],
+    },
+    email: {
+      type: String,
+      required: [true, "Email is required"],
+      unique: true,
+      lowercase: true,
+      trim: true,
+    },
+    password: {
+      type: String,
+      required: [true, "Password is required"],
+      minlength: [6, "Password must be at least 6 characters long"],
+    },
+    cartItems: [
+      {
+        quantity: {
+          type: Number,
+          default: 1,
+        },
+        product: {
+          type: mongoose.Schema.Types.ObjectId,
+          ref: "Product",
+        },
+      },
+    ],
+    role: {
+      type: String,
+      enum: ["customer", "admin"],
+      default: "customer",
+    },
+    //createdAt, updatedAt object
+  },
+  { timestaps: true }
+);
+
+//create a model of the schema
+const User = mongoose.model("user", userSchema);
+
+export default User;
