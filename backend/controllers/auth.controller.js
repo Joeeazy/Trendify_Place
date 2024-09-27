9506557a66624338ed15b0d6a19d128842618df0
Joeeazy <joeeazy34@gmail.com>
2024-09-27 13:32:31 +0300

    Refresh Access Token

--- backend/controllers/auth.controller.js
+++ backend/controllers/auth.controller.js
@@ -133,3 +133,41 @@ export const logout = async (req, res) => {
     res.status(500).json({ message: "server error", error: error.message });
   }
 };
+
+export const refreshToken = async (req, res) => {
+  try {
+    //check if refresh Token exists
+    const refreshToken = req.cookies.refreshToken;
+    if (!refreshToken) {
+      return res.status(401).json({ message: "No refresh token found" });
+    }
+    //else Verify token using secret key
+    const decoded = jwt.verify(refreshToken, process.env.REFRESH_TOKEN_SECRET);
+    //compare refreshtoken provided with that stored in redisdb
+    const storedToken = await redis.get(`refresh_token: ${decoded.userId}`);
+
+    //if the dont match
+    if (storedToken !== refreshToken) {
+      return res.status(401).json({ message: "Invalid refresh token" });
+    }
+
+    //if refresh token is valid enerate new access token
+    const accessToken = jwt.sign(
+      { userId: decoded.userId },
+      process.env.ACCESS_TOKEN_SECRET,
+      { expiresIn: "15m" }
+    );
+
+    res.cookie("accessToken", accessToken, {
+      httpOnly: true,
+      secure: process.env.NODE_ENV === "production",
+      samesize: "strict",
+      maxAge: 15 * 60 * 1000,
+    });
+
+    res.json({ message: "Token Refreshed Successfully" });
+  } catch (error) {
+    console.log("error in the refresh access token controller", error.message);
+    res.status(500).jsone({ message: "Server error", error: error.message });
+  }
+};
