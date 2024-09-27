05cb7ac6fe1eca81ab43a413199803fed0cd889a
Joeeazy <joeeazy34@gmail.com>
2024-09-27 09:36:15 +0300

    user authentication done cookies set

--- backend/controllers/auth.controller.js
+++ backend/controllers/auth.controller.js
@@ -1,4 +1,49 @@
 import User from "../models/user.model.js";
+import jwt from "jsonwebtoken";
+import { redis } from "../lib/redis.js";
+
+//create two diffrent tokens save the refreshtoken to the redisdb
+const generateTokens = (userId) => {
+  //get an access token which expires in 15mins
+  const accessToken = jwt.sign({ userId }, process.env.ACCESS_TOKEN_SECRET, {
+    expiresIn: "15m",
+  });
+
+  //get an refresh token which expires in 7days
+  const refreshToken = jwt.sign({ userId }, process.env.REFRESH_TOKEN_SECRET, {
+    expiresIn: "7d",
+  });
+
+  return { accessToken, refreshToken };
+};
+
+//store the refreshToken in redisdb
+const storeRefreshToken = async (userId, refreshToken) => {
+  await redis.set(
+    `refresh_token: ${userId}`,
+    refreshToken,
+    "EX",
+    7 * 24 * 60 * 60
+  ); //expires in 7days
+};
+
+//fucntion to set cookies
+const setCookies = (res, accessToken, refreshToken) => {
+  res.cookie("accessToken", accessToken, {
+    //make more secure
+    httpOnly: true, //prevents XSS attacks , cross site scripting attacks
+    secure: process.env.NODE_ENV === "production",
+    sameSite: "strict", //prevents CSRF attacks, Cross-site request forgery
+    maxAge: 15 * 60 * 1000, //15 minutes in miliseconds
+  });
+  res.cookie("refreshToken", refreshToken, {
+    //make more secure
+    httpOnly: true, //prevents XSS attacks , cross site scripting attacks
+    secure: process.env.NODE_ENV === "production",
+    sameSite: "strict", //prevents CSRF attacks, Cross-site request forgery
+    maxAge: 7 * 24 * 60 * 60 * 1000, //7days in miliseconds
+  });
+};
 
 export const signup = async (req, res) => {
   const { email, password, name } = req.body;
@@ -14,9 +59,24 @@ export const signup = async (req, res) => {
     const user = await User.create({ name, email, password });
 
     //authenticate user
+    //create two diffrenet tokens access token and a refresh token
+    const { accessToken, refreshToken } = generateTokens(user._id);
+    //store the refreshcookie
+    await storeRefreshToken(user._id, refreshToken);
+
+    //set tokens into a cookie
+    setCookies(res, accessToken, refreshToken);
 
     //send a message
-    res.status(201).json({ user, message: "user Created Successfully" });
+    res.status(201).json({
+      user: {
+        _id: user._id,
+        name: user.name,
+        email: user.email,
+        role: user.role,
+      },
+      message: "user Created Successfully",
+    });
   } catch (error) {
     res.status(500).json({ message: error.message });
   }
