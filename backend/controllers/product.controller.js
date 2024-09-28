cd643a9916fdbf1e48cf0b8e9be672ef74424324
Joeeazy <joeeazy34@gmail.com>
2024-09-28 13:27:45 +0300

    getFeaturedProducts controller done

--- backend/controllers/product.controller.js
+++ backend/controllers/product.controller.js
@@ -1,3 +1,4 @@
+import { redis } from "../lib/redis.js";
 import Product from "../models/product.model.js";
 
 //fucntion to fetch all products for the admin profile
@@ -9,6 +10,34 @@ export const getAllProducts = async (req, res) => {
     res.json({ products });
   } catch (error) {
     console.log("Error in getAllProducts controller", error.message);
+    res.status(500).json({ message: "Server Error", error: error.message });
+  }
+};
+
+export const getFeaturedProducts = async (req, res) => {
+  try {
+    //store to mongodb and redisdb for fast access(redis cache)
+    let featuredProducts = await redis.get("featured_products");
+    if (featuredProducts) {
+      //redis returns the featuredProducts as a string
+      return res.json(JSON.parse(featuredProducts));
+    }
+
+    // if the featured_products aare no present in redisdb get them from mongdb
+    //.lean() = returns plain js objects instead of mongoDb objects
+    //which is good for performance
+    featuredProducts = await Product.find({ isFeatured: true }).lean();
+
+    if (!featuredProducts) {
+      return res.status(404).json({ meesage: "No featured proucts found" });
+    }
+
+    //if we have the products store in redis for quick acceess
+    await redis.set("featured_products"), JSON.stringify(featuredProducts);
+
+    res.json(featuredProducts);
+  } catch (error) {
+    console.log("Error in the getFeaturedProducts controller", error.message);
     res.status(500).json({ message: "Server Error", error: error.message });
   }
 };
--- backend/routes/product.route.js
+++ backend/routes/product.route.js
@@ -1,9 +1,15 @@
 import express from "express";
-import { getAllProducts } from "../controllers/product.controller.js";
+import {
+  getAllProducts,
+  getFeaturedProducts,
+} from "../controllers/product.controller.js";
 import { protectRoute, adminRoute } from "../middleware/auth.middleware.js";
 
 const router = express.Router();
 
+//for the admin only route
 router.get("/", protectRoute, adminRoute, getAllProducts);
+// viewed by all customers
+router.get("/featured", getFeaturedProducts);
 
 export default router;
