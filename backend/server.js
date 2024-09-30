61f17501a3981796f6f4d04e217f0841d80893dd
Joeeazy <joeeazy34@gmail.com>
2024-09-30 12:12:29 +0300

    coupons model created

--- backend/server.js
+++ backend/server.js
@@ -6,6 +6,7 @@ import cookieParser from "cookie-parser";
 import authRoutes from "./routes/auth.route.js";
 import productRoutes from "./routes/product.route.js";
 import cartRoutes from "./routes/cart.route.js";
+import couponRoutes from "./routes/coupon.route.js";
 
 import { connectDB } from "./lib/db.js";
 
@@ -21,9 +22,11 @@ app.use(cookieParser()); //enables manipulation of cookies
 //authentication
 app.use("/api/auth", authRoutes);
 
-app.use("/api/product", productRoutes);
+app.use("/api/products", productRoutes);
 
 app.use("/api/cart", cartRoutes);
+
+app.use("/api/coupons", couponRoutes);
 
 app.listen(PORT, () => {
   console.log(`Server is running on http://localhost:${PORT}`);
