10571150aaf821528dd3a61277928c829e014585
Joeeazy <joeeazy34@gmail.com>
2024-10-02 20:05:21 +0300

    getUsersProfile functionality

--- backend/routes/auth.route.js
+++ backend/routes/auth.route.js
@@ -5,7 +5,9 @@ import {
   login,
   logout,
   refreshToken,
+  getProfile,
 } from "../controllers/auth.controller.js";
+import { protectRoute } from "../middleware/auth.middleware.js";
 
 const router = express.Router();
 
@@ -22,6 +24,6 @@ router.post("/logout", logout);
 router.post("/refresh-token", refreshToken);
 
 // get users profile route
-//router.get("/profile", getProfile);
+router.get("/profile", protectRoute, getProfile);
 
 export default router;
